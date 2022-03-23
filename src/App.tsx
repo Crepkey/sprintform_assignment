5f16519473625e8aafdfd5e8c85c1dac753aa39c
Crepkey <crepkey@gmail.com>
2022-03-23 13:31:09 +0100

    The initial version of cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import styled from "styled-components";
 import PageTitle from "./comps/common/PageTitle";
+import { colors } from "./utils/colors";
+import { breakePoints } from "./utils/utils";
 
 const MainContainer = styled.div`
 	display: flex;
@@ -10,10 +12,32 @@ const MainContainer = styled.div`
 	width: 100vw;
 `;
 
+const CardContainer = styled.div`
+	display: flex;
+`;
+
+const Card = styled.div`
+	display: flex;
+	width: 80px;
+	height: 80px;
+	border: 1px solid ${colors.cardBorder};
+	border-radius: 5px;
+	padding: 10px;
+	transition: all 0.3s ease;
+	:hover {
+		margin-left: 0;
+		cursor: pointer;
+		border-left: 3px solid ${colors.red};
+	}
+`;
+
 function App() {
 	return (
 		<MainContainer>
 			<PageTitle>FIZZ BUZZ FINDER</PageTitle>
+			<CardContainer>
+				<Card>1</Card>
+			</CardContainer>
 		</MainContainer>
 	);
 }
